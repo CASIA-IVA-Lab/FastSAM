40554429f9484adacba71b8cff8059adbe38c6d2
duyinglong <d1367670890@gmail.com>
2023-06-21 15:42:04 +0800

    add point prompt founction

--- Inference.py
+++ Inference.py
@@ -3,13 +3,13 @@ import sys
 from utils.tools import *
 import os
 import argparse
-
+import ast
 
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default='../finally.pt', help='model')
+    parser.add_argument('--model_path', type=str, default='./weights/FastSAM.pt', help='model')
     parser.add_argument('--img_path', type=str, default='./images/dogs.jpg',help='path to image file')
     parser.add_argument('--imgsz', type=int, default=1024,help='path to image file')
     parser.add_argument('--iou', type=float, default=0.9, help='iou threshold for filtering the annotations')
@@ -18,9 +18,13 @@ def parse_args():
     parser.add_argument('--output', type=str, default='./output/', help='image save path')
     parser.add_argument('--randomcolor', type=bool, default=True, help='mask random color')
     parser.add_argument('--point_prompt', type=str, default="[[0,0]]", help='[[x,y]]')
+    parser.add_argument('--point_label', type=str, default="[0]", help='[[x,y]]')
     parser.add_argument('--box_prompt', type=str, default="[0,0,0,0]", help='x,y,w,h')
+    parser.add_argument('--better_quality', type=str, default=False, help='better quality use cpu')
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     parser.add_argument('--device', type=str, default=device)
+    parser.add_argument('--retina', type=bool, default=True, help='text')
+    parser.add_argument('--withContours', type=bool, default=True, help='text')
     return parser.parse_args()
 
 
@@ -28,64 +32,42 @@ def parse_args():
 def main(args):
     # load model
     model = YOLO(args.model_path)
-    args.point_prompt = eval(args.point_prompt)
-    args.box_prompt = eval(args.box_prompt)
-    results = model(args.img_path, imgsz=args.imgsz, device=args.device, retina_masks=True, iou=args.iou, conf=args.conf,
+    args.point_prompt = ast.literal_eval(args.point_prompt)
+    args.box_prompt = ast.literal_eval(args.box_prompt)
+    args.point_label = ast.literal_eval(args.point_label)
+    results = model(args.img_path, imgsz=args.imgsz, device=args.device, retina_masks=args.retina, iou=args.iou, conf=args.conf,
                          max_det=100)
     if args.box_prompt[2] != 0 and args.box_prompt[3] != 0:
         annotations = prompt(results, args,box=True)
-        image_name = os.path.basename(args.img_path)
-        post_process(annotations=annotations, image_path=args.img_path, mask_random_color=args.randomcolor,save_path=args.output, 
-                     result_name=image_name,bbox=convert_box_xywh_to_xyxy(args.box_prompt))
+        annotations = np.array([annotations])
+        fast_process(annotations=annotations, args=args, mask_random_color=args.randomcolor,bbox=convert_box_xywh_to_xyxy(args.box_prompt))
 
     elif args.text_prompt!= None:
         results = format_results(results[0],0)
         annotations = prompt(results, args,text=True)
-        image_name = os.path.basename(args.img_path)
-        post_process(annotations=annotations, image_path=args.img_path, mask_random_color=args.randomcolor,
-                     save_path=args.output, result_name=image_name)
+        annotations = np.array([annotations])
+        fast_process(annotations=annotations, args=args, mask_random_color=args.randomcolor)
 
     elif args.point_prompt[0] != [0,0]:
         results = format_results(results[0],0)
         annotations = prompt(results, args,point=True)
-        image_name = os.path.basename(args.img_path)
-        post_process(annotations=annotations, image_path=args.img_path, mask_random_color=args.randomcolor,save_path=args.output, 
-                     result_name=image_name,points=args.point_prompt)
+        # list to numpy
+        annotations = np.array([annotations])
+        print(annotations.shape)
+        fast_process(annotations=annotations, args=args, mask_random_color=args.randomcolor,points=args.point_prompt)
 
     else:
-        results = format_results(results[0], 100)
-        annotations = results
-        #annotations, _ = filter_masks(results)
-        image_name = os.path.basename(args.img_path)
-        post_process(annotations=annotations, image_path=args.img_path, mask_random_color=args.randomcolor,save_path=args.output, result_name=image_name)
-    
+        fast_process(annotations=results[0].masks.data, args=args, mask_random_color=args.randomcolor)
 def prompt(results, args,box=None, point=None, text=None):
-    annotations = []
     if box:
         mask, idx = box_prompt(results[0].masks.data, convert_box_xywh_to_xyxy(args.box_prompt))
     elif point:
-        mask,idx = point_prompt(results, args.point_prompt)
+        mask,idx = point_prompt(results, args.point_prompt, args.point_label)
     elif text:
         mask, idx = text_prompt(results, args)
     else:
-        return annotations
-    annotation = {}
-    annotation['id'] = 0
-
-    
-    if box:
-        annotation['segmentation'] = mask.cpu().numpy()
-        annotation['bbox'] = results[0].boxes.data[idx]
-        annotation['score'] = results[0].boxes.conf[idx]
-        annotation['area'] = annotation['segmentation'].sum()
-    else:
-        annotation['segmentation'] = mask
-        annotation['bbox'] = results[idx]['bbox']
-        annotation['score'] = results[idx]['score']
-        annotation['area'] = annotation['segmentation'].sum()
-
-    annotations.append(annotation)
-    return annotations
+        return None
+    return mask
 
 
 if __name__ == '__main__':
--- utils/tools.py
+++ utils/tools.py
@@ -1,11 +1,12 @@
-
 import numpy as np
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 import cv2
 import torch
 import os
+import time
 import clip
+
 
 def convert_box_xywh_to_xyxy(box):
     x1 = box[0]
@@ -19,24 +20,24 @@ def segment_image(image, bbox):
     image_array = np.array(image)
     segmented_image_array = np.zeros_like(image_array)
     x1, y1, x2, y2 = bbox
-    segmented_image_array[y1:y2,x1:x2] = image_array[y1:y2,x1:x2]
+    segmented_image_array[y1:y2, x1:x2] = image_array[y1:y2, x1:x2]
     segmented_image = Image.fromarray(segmented_image_array)
     black_image = Image.new("RGB", image.size, (255, 255, 255))
-    #transparency_mask = np.zeros_like((), dtype=np.uint8)
-    transparency_mask = np.zeros((image_array.shape[0],image_array.shape[1]), dtype=np.uint8)
-    transparency_mask[y1:y2,x1:x2] = 255
+    # transparency_mask = np.zeros_like((), dtype=np.uint8)
+    transparency_mask = np.zeros((image_array.shape[0], image_array.shape[1]), dtype=np.uint8)
+    transparency_mask[y1:y2, x1:x2] = 255
     transparency_mask_image = Image.fromarray(transparency_mask, mode='L')
     black_image.paste(segmented_image, mask=transparency_mask_image)
     return black_image
 
-def format_results(result,filter = 0):
+
+def format_results(result, filter=0):
     annotations = []
     n = len(result.masks.data)
     for i in range(n):
         annotation = {}
         mask = result.masks.data[i] == 1.0
 
-    
         if torch.sum(mask) < filter:
             continue
         annotation['id'] = i
@@ -47,7 +48,8 @@ def format_results(result,filter = 0):
         annotations.append(annotation)
     return annotations
 
-def filter_masks(annotations):       # filte the overlap mask
+
+def filter_masks(annotations):  # filte the overlap mask
     annotations.sort(key=lambda x: x['area'], reverse=True)
     to_remove = set()
     for i in range(0, len(annotations)):
@@ -60,7 +62,8 @@ def filter_masks(annotations):       # filte the overlap mask
                     if (a['segmentation'] & b['segmentation']).sum() / b['segmentation'].sum() > 0.8:
                         to_remove.add(j)
 
-    return [a for i, a in enumerate(annotations) if i not in to_remove],to_remove
+    return [a for i, a in enumerate(annotations) if i not in to_remove], to_remove
+
 
 def get_bbox_from_mask(mask):
     mask = mask.astype(np.uint8)
@@ -79,43 +82,145 @@ def get_bbox_from_mask(mask):
         w = x2 - x1
     return [x1, y1, x2, y2]
 
-def show_mask(annotation, ax,random_color=False,bbox=None,points=None):
-    if random_color :    # random mask color
-        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+
+def fast_process(annotations, args, mask_random_color, bbox=None, points=None, edgs=False):
+    if isinstance(annotations[0], dict):
+        annotations = [annotation['segmentation'] for annotation in annotations]
+    result_name = os.path.basename(args.img_path)
+    image = cv2.imread(args.img_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    original_h = image.shape[0]
+    original_w = image.shape[1]
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image)
+    if args.better_quality == True:
+        if isinstance(annotations[0], torch.Tensor):
+            annotations = np.array(annotations.cpu())
+        for i, mask in enumerate(annotations):
+            mask = cv2.morphologyEx(mask.astype(np.uint8), cv2.MORPH_CLOSE, np.ones((3, 3), np.uint8))
+            annotations[i] = cv2.morphologyEx(mask.astype(np.uint8), cv2.MORPH_OPEN, np.ones((8, 8), np.uint8))
+    if args.device == 'cpu':
+        annotations = np.array(annotations)
+        fast_show_mask(annotations,
+                       plt.gca(),
+                       random_color=mask_random_color, bbox=bbox,
+                       points=points,
+                       pointlabel=args.point_label,
+                       retinamask=args.retina,
+                       target_height=original_h,
+                       target_width=original_w)
     else:
-        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
-    if type(annotation) == dict:
-        annotation = annotation['segmentation']
-    mask = annotation
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    # draw box
+        if isinstance(annotations[0], np.ndarray):
+            annotations = torch.from_numpy(annotations)
+        fast_show_mask_gpu(annotations,
+                           plt.gca(),
+                           random_color=args.randomcolor,
+                           bbox=bbox,
+                           points=points,
+                           pointlabel=args.point_label,
+                           retinamask=args.retina,
+                           target_height=original_h,
+                           target_width=original_w)
+    if isinstance(annotations, torch.Tensor):
+        annotations = annotations.cpu().numpy()
+    if args.withContours == True:
+        contour_all = []
+        temp = np.zeros((original_h, original_w, 1))
+        for i, mask in enumerate(annotations):
+            if type(mask) == dict:
+                mask = mask['segmentation']
+            annotation = mask.astype(np.uint8)
+            if args.retina == False:
+                annotation = cv2.resize(annotation, (original_w, original_h), interpolation=cv2.INTER_NEAREST)
+            contours, hierarchy = cv2.findContours(annotation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            for contour in contours:
+                contour_all.append(contour)
+        cv2.drawContours(temp, contour_all, -1, (255, 255, 255), 2)
+        color = np.array([0 / 255, 0 / 255, 255 / 255, 0.8])
+        contour_mask = temp / 225 * color.reshape(1, 1, -1)
+        plt.imshow(contour_mask)
+
+    save_path = args.output
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    plt.axis('off')
+    plt.savefig(save_path + result_name, bbox_inches='tight', pad_inches=0.0)
+
+
+#   CPU post process
+def fast_show_mask(annotation, ax, random_color=False, bbox=None, points=None, pointlabel=None, retinamask=True,
+                   target_height=960, target_width=960):
+    msak_sum = annotation.shape[0]
+    height = annotation.shape[1]
+    weight = annotation.shape[2]
+    # 将annotation 按照面积 排序
+    areas = np.sum(annotation, axis=(1, 2))
+    sorted_indices = np.argsort(areas)[::-1]
+    annotation = annotation[sorted_indices]
+
+    index = (annotation != 0).argmax(axis=0)
+    color = np.random.random((msak_sum, 1, 1, 3))
+    transparency = np.ones((msak_sum, 1, 1, 1)) * 0.6
+    visual = np.concatenate([color, transparency], axis=-1)
+    mask_image = np.expand_dims(annotation, -1) * visual
+
+    show = np.zeros((height, weight, 4))
+
+    h_indices, w_indices = np.meshgrid(np.arange(height), np.arange(weight), indexing='ij')
+    indices = (index[h_indices, w_indices], h_indices, w_indices, slice(None))
+    # 使用向量化索引更新show的值
+    show[h_indices, w_indices, :] = mask_image[indices]
     if bbox is not None:
         x1, y1, x2, y2 = bbox
         ax.add_patch(plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, edgecolor='b', linewidth=1))
     # draw point
     if points is not None:
-        ax.scatter([point[0] for point in points], [point[1] for point in points], s=10, c='g')
-    ax.imshow(mask_image)
-    return mask_image
+        plt.scatter([point[0] for i, point in enumerate(points) if pointlabel[i] == 1],
+                    [point[1] for i, point in enumerate(points) if pointlabel[i] == 1], s=20, c='y')
+        plt.scatter([point[0] for i, point in enumerate(points) if pointlabel[i] == 0],
+                    [point[1] for i, point in enumerate(points) if pointlabel[i] == 0], s=20, c='m')
 
-def post_process(annotations,image_path,mask_random_color,save_path,result_name,bbox=None,points=None):
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    plt.figure(figsize=(10, 10))
-    plt.imshow(image)
-    for i, mask in enumerate(annotations):
-        show_mask(mask, plt.gca(),random_color=mask_random_color,bbox=bbox,points=points)
-    plt.axis('off')
+    if retinamask == False:
+        show = cv2.resize(show, (target_width, target_height), interpolation=cv2.INTER_NEAREST)
+    ax.imshow(show)
 
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-    plt.savefig(os.path.join(save_path, result_name), bbox_inches='tight', pad_inches=0.0)
+
+def fast_show_mask_gpu(annotation, ax, random_color=False, bbox=None, points=None, pointlabel=None, retinamask=True,
+                       target_height=960, target_width=960):
+    msak_sum = annotation.shape[0]
+    height = annotation.shape[1]
+    weight = annotation.shape[2]
+    areas = torch.sum(annotation, dim=(1, 2))
+    sorted_indices = torch.argsort(areas, descending=True)
+    annotation = annotation[sorted_indices]
+    # 找每个位置第一个非零值下标
+    index = (annotation != 0).to(torch.long).argmax(dim=0)
+    color = torch.rand((msak_sum, 1, 1, 3)).to(annotation.device)
+    transparency = torch.ones((msak_sum, 1, 1, 1)).to(annotation.device) * 0.6
+    visual = torch.cat([color, transparency], dim=-1)
+    mask_image = torch.unsqueeze(annotation, -1) * visual
+    # 按index取数，index指每个位置选哪个batch的数，把mask_image转成一个batch的形式
+    show = torch.zeros((height, weight, 4)).to(annotation.device)
+    h_indices, w_indices = torch.meshgrid(torch.arange(height), torch.arange(weight))
+    indices = (index[h_indices, w_indices], h_indices, w_indices, slice(None))
+    # 使用向量化索引更新show的值
+    show[h_indices, w_indices, :] = mask_image[indices]
+    show_cpu = show.cpu().numpy()
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        ax.add_patch(plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, edgecolor='b', linewidth=1))
+    # draw point
+    if points is not None:
+        plt.scatter([point[0] for i, point in enumerate(points) if pointlabel[i] == 1],
+                    [point[1] for i, point in enumerate(points) if pointlabel[i] == 1], s=20, c='y')
+        plt.scatter([point[0] for i, point in enumerate(points) if pointlabel[i] == 0],
+                    [point[1] for i, point in enumerate(points) if pointlabel[i] == 0], s=20, c='m')
+    ax.imshow(show_cpu)
 
 
 # clip
 @torch.no_grad()
-def retriev(model,preprocess,elements: [Image.Image], search_text: str,device) -> int:
+def retriev(model, preprocess, elements: [Image.Image], search_text: str, device) -> int:
     preprocessed_images = [preprocess(image).to(device) for image in elements]
     tokenized_text = clip.tokenize([search_text]).to(device)
     stacked_images = torch.stack(preprocessed_images)
@@ -127,24 +232,25 @@ def retriev(model,preprocess,elements: [Image.Image], search_text: str,device) -
     return probs[:, 0].softmax(dim=0)
 
 
-def crop_image(annotations,image_path):
+def crop_image(annotations, image_path):
     image = Image.open(image_path)
     cropped_boxes = []
     cropped_images = []
     not_crop = []
     filter_id = []
-    #annotations, _ = filter_masks(annotations)
-    #filter_id = list(_)
+    # annotations, _ = filter_masks(annotations)
+    # filter_id = list(_)
     for _, mask in enumerate(annotations):
         if np.sum(mask["segmentation"]) <= 100:
             filter_id.append(_)
             continue
-        bbox = get_bbox_from_mask(mask["segmentation"])     # mask 的 bbox
-        cropped_boxes.append(segment_image(image, bbox))              # 保存裁剪的图片
-        #cropped_boxes.append(segment_image(image,mask["segmentation"]))
-        cropped_images.append(bbox)                         # 保存裁剪的图片的bbox
+        bbox = get_bbox_from_mask(mask["segmentation"])  # mask 的 bbox
+        cropped_boxes.append(segment_image(image, bbox))  # 保存裁剪的图片
+        # cropped_boxes.append(segment_image(image,mask["segmentation"]))
+        cropped_images.append(bbox)  # 保存裁剪的图片的bbox
 
     return cropped_boxes, cropped_images, not_crop, filter_id, annotations
+
 
 def box_prompt(masks, bbox):
     h = masks.shape[1]
@@ -154,7 +260,7 @@ def box_prompt(masks, bbox):
     bbox[2] = round(bbox[2]) if round(bbox[2]) < w else w
     bbox[3] = round(bbox[3]) if round(bbox[3]) < h else h
 
-    #IoUs = torch.zeros(len(masks), dtype=torch.float32)
+    # IoUs = torch.zeros(len(masks), dtype=torch.float32)
     bbox_area = ((bbox[3] - bbox[1]) * (bbox[2] - bbox[0]))
 
     masks_area = torch.sum(masks[:, bbox[1]:bbox[3], bbox[0]:bbox[2]], dim=(1, 2))
@@ -164,21 +270,23 @@ def box_prompt(masks, bbox):
     IoUs = masks_area / union
     max_iou_index = torch.argmax(IoUs)
 
-    return masks[max_iou_index], max_iou_index
+    return masks[max_iou_index].cpu().numpy(), max_iou_index
 
-def point_prompt(masks, points): # numpy 处理
+
+def point_prompt(masks, points, pointlabel):  # numpy 处理
     onemask = np.zeros((masks[0]['segmentation'].shape[0], masks[0]['segmentation'].shape[1]))
     for i, annotation in enumerate(masks):
         if type(annotation) == dict:
             mask = annotation['segmentation']
         else:
             mask = annotation
-        for point in points:
-            if mask[point[1],point[0]]==1:
+        for i, point in enumerate(points):
+            if mask[point[1], point[0]] == 1 and pointlabel[i] == 1:
                 onemask += mask
-            
-    onemask = onemask.clip(0, 1)
-    return onemask,0
+            if mask[point[1], point[0]] == 1 and pointlabel[i] == 0:
+                onemask -= mask
+    onemask = onemask >= 1
+    return onemask, 0
 
 
 def text_prompt(annotations, args):
