96e737cae0424f063412a85e722ccd661b6165f1
Wenchao Ding <40165666+berry-ding@users.noreply.github.com>
2023-06-28 15:43:31 +0800

    Update prompt.py

--- fastsam/prompt.py
+++ fastsam/prompt.py
@@ -79,7 +79,7 @@ class FastSAMPrompt:
         if len(contours) > 1:
             for b in contours:
                 x_t, y_t, w_t, h_t = cv2.boundingRect(b)
-                # 将多个bbox合并成一个
+                # Merge multiple bounding boxes into one.
                 x1 = min(x1, x_t)
                 y1 = min(y1, y_t)
                 x2 = max(x2, x_t + w_t)
@@ -202,7 +202,7 @@ class FastSAMPrompt:
         msak_sum = annotation.shape[0]
         height = annotation.shape[1]
         weight = annotation.shape[2]
-        # 将annotation 按照面积 排序
+        #Sort annotations based on area.
         areas = np.sum(annotation, axis=(1, 2))
         sorted_indices = np.argsort(areas)
         annotation = annotation[sorted_indices]
@@ -219,7 +219,7 @@ class FastSAMPrompt:
         show = np.zeros((height, weight, 4))
         h_indices, w_indices = np.meshgrid(np.arange(height), np.arange(weight), indexing='ij')
         indices = (index[h_indices, w_indices], h_indices, w_indices, slice(None))
-        # 使用向量化索引更新show的值
+        # Use vectorized indexing to update the values of 'show'.
         show[h_indices, w_indices, :] = mask_image[indices]
         if bbox is not None:
             x1, y1, x2, y2 = bbox
@@ -261,7 +261,7 @@ class FastSAMPrompt:
         areas = torch.sum(annotation, dim=(1, 2))
         sorted_indices = torch.argsort(areas, descending=False)
         annotation = annotation[sorted_indices]
-        # 找每个位置第一个非零值下标
+        # Find the index of the first non-zero value at each position.
         index = (annotation != 0).to(torch.long).argmax(dim=0)
         if random_color:
             color = torch.rand((msak_sum, 1, 1, 3)).to(annotation.device)
@@ -271,11 +271,11 @@ class FastSAMPrompt:
         transparency = torch.ones((msak_sum, 1, 1, 1)).to(annotation.device) * 0.6
         visual = torch.cat([color, transparency], dim=-1)
         mask_image = torch.unsqueeze(annotation, -1) * visual
-        # 按index取数，index指每个位置选哪个batch的数，把mask_image转成一个batch的形式
+        # Select data according to the index. The index indicates which batch's data to choose at each position, converting the mask_image into a single batch form.
         show = torch.zeros((height, weight, 4)).to(annotation.device)
         h_indices, w_indices = torch.meshgrid(torch.arange(height), torch.arange(weight), indexing='ij')
         indices = (index[h_indices, w_indices], h_indices, w_indices, slice(None))
-        # 使用向量化索引更新show的值
+        # Use vectorized indexing to update the values of 'show'.
         show[h_indices, w_indices, :] = mask_image[indices]
         show_cpu = show.cpu().numpy()
         if bbox is not None:
@@ -331,9 +331,9 @@ class FastSAMPrompt:
                 filter_id.append(_)
                 continue
             bbox = self._get_bbox_from_mask(mask['segmentation'])  # mask 的 bbox
-            cropped_boxes.append(self._segment_image(image, bbox))  # 保存裁剪的图片
+            cropped_boxes.append(self._segment_image(image, bbox))  
             # cropped_boxes.append(segment_image(image,mask["segmentation"]))
-            cropped_images.append(bbox)  # 保存裁剪的图片的bbox
+            cropped_images.append(bbox)  # Save the bounding box of the cropped image.
 
         return cropped_boxes, cropped_images, not_crop, filter_id, annotations
 
@@ -368,7 +368,7 @@ class FastSAMPrompt:
 
         return np.array([masks[max_iou_index].cpu().numpy()])
 
-    def point_prompt(self, points, pointlabel):  # numpy 处理
+    def point_prompt(self, points, pointlabel):  # numpy 
 
         masks = self._format_results(self.results[0], 0)
         target_height = self.ori_img.shape[0]
