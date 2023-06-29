dca3d30855fcb76268c3d041e8481e60cdffd921
AAAAAAAyq <769753199@qq.com>
2023-06-29 02:34:13 +0000

    backroll

--- fastsam/__init__.py
+++ fastsam/__init__.py
@@ -3,6 +3,7 @@
 from .model import FastSAM
 from .predict import FastSAMPredictor
 from .prompt import FastSAMPrompt
+from .val import FastSAMValidator
 from .decoder import FastSAMDecoder
 
-__all__ = 'FastSAMPredictor', 'FastSAM', 'FastSAMPrompt', 'FastSAMDecoder'
+__all__ = 'FastSAMPredictor', 'FastSAM', 'FastSAMPrompt', 'FastSAMValidator', 'FastSAMDecoder'
