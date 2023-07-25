37c7a3f269b6c547134e9f8226583de1cab7572a
schulze <schulze@fzi.de>
2023-07-25 10:16:44 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from setuptools import find_packages, setup
+
+setup(
+    name="fastsam",
+    version="0.1.0",
+    install_requires=[],
+    description="",
+    url="https://github.com/CASIA-IVA-Lab/FastSAM",
+    packages=find_packages(exclude=["assets"])
+)
