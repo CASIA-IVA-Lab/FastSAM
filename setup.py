0e3a9360bc1a36513b8fe0e4d7f5aa2de84b80d3
christopher5106 <christopher.bourez@gmail.com>
2023-10-18 17:54:08 +0200

    n

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@
 from setuptools import find_packages, setup
 
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+REQUIREMENTS += [
+    "CLIP @ git+https://github.com/openai/CLIP.git@a1d071733d7111c9c014f024669f959182114e33#egg=CLIP"
+]
+
 setup(
     name="fastsam",
     version="0.1.1",
@@ -13,5 +17,5 @@ setup(
         "fastsam": "fastsam",
         "fastsam_tools": "utils",
     },
-    url="https://github.com/CASIA-IVA-Lab/FastSAM",
+    url="https://github.com/CASIA-IVA-Lab/FastSAM"
 )
