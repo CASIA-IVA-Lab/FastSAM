2e2d1c007b5f25196b2c5ab76614cd84917e7377
christopher5106 <christopher.bourez@gmail.com>
2023-10-18 17:40:40 +0200

    n

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 
 from setuptools import find_packages, setup
 
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 setup(
     name="fastsam",
     version="0.1.1",
-    install_requires=[],
+    install_requires=REQUIREMENTS,
     packages=["fastsam", "fastsam_tools"],
     package_dir= {
         "fastsam": "fastsam",
